ec055008afb41eee7657dc64cbe5f5d5955739a3
francescolazzarin <francesco.lazzarin1996@gmail.com>
2024-01-28 10:54:50 +0100

    <feat>:Aggiunte funzioni per:immagine corrente, scorrimento immagine al click,autoplay e stop autoplay all'over

---
+++ js/script.js
@@ -0,0 +1,94 @@
+// const slides = [
+// {
+//     image: 'img/01.webp',
+//         title: 'Marvel\'s Spiderman Miles Morale',
+//         text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+//     }, {
+//         image: 'img/02.webp',
+//         title: 'Ratchet & Clank: Rift Apart',
+//         text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+//     }, {
+//         image: 'img/03.webp',
+//         title: 'Fortnite',
+//         text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+//     }, {
+//         image: 'img/04.webp',
+//         title: 'Stray',
+//         text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+//     }, {
+//         image: 'img/05.webp',
+//         title: "Marvel's Avengers",
+//         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+//     }
+// ];
+
+// console.log(slides);
+const { createApp } = Vue
+
+  createApp({
+    data() {
+        return {
+            autoplayinterval:null,
+            indiceCorrente:0,
+            slides : [
+                {
+                    image: 'img/01.webp',
+                    title: 'Marvel\'s Spiderman Miles Morale',
+                    text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+                },
+                {
+                    image: 'img/02.webp',
+                    title: 'Ratchet & Clank: Rift Apart',
+                    text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+                }, 
+                {
+                    image: 'img/03.webp',
+                    title: 'Fortnite',
+                    text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+                }, 
+                {
+                    image: 'img/04.webp',
+                    title: 'Stray',
+                    text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+                }, 
+                {
+                    image: 'img/05.webp',
+                    title: "Marvel's Avengers",
+                    text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+                },
+            ]
+       
+        }
+
+    },
+    methods:{
+        prev(){
+            if (this.indiceCorrente<=0){
+                this.indiceCorrente=this.slides.length - 1
+            }else{
+                this.indiceCorrente--
+            }
+        },
+        next(){
+            if(this.indiceCorrente>=this.slides.length -1 ){
+                this.indiceCorrente=0
+            }else{
+                this.indiceCorrente++
+            }
+        },
+        immagineCorrente(index){
+           this.indiceCorrente=index
+        },
+        autoplay() {
+            this.autoplayinterval = setInterval(() => {
+              this.next();
+            }, 3000);
+          },
+        bloccaAutoplay(){
+            clearInterval(this.autoplayinterval)
+        }
+    },
+    created(){
+        this.autoplay()
+    }
+  }).mount('#app')
